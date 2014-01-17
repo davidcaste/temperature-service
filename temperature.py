08a00f2cca32a977056c67a18bf28884bbb585a0
dcastellanos <dcastellanos@indra.es>
2014-01-18 00:19:07 +0100

    Save temperature samples in a daily log

--- temperature.py
+++ temperature.py
@@ -3,24 +3,27 @@
 import glob
 import time
 import pywapi
+import os
 import sys
+import csv
+import datetime
 
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+BASE_DIR = '/sys/bus/w1/devices/'
+DEVICE_FOLDER = glob.glob(BASE_DIR + '28*')[0]
+DEVICE_FILE = DEVICE_FOLDER + '/w1_slave'
 
 def get_yahoo_temp():
 	result = pywapi.get_weather_from_yahoo('SPXX0050', 'metric')
 	return float(result['condition']['temp'])
 
 def read_temp_raw():
-	f = open(device_file, 'r')
+	f = open(DEVICE_FILE, 'r')
 	lines = f.readlines()
 	f.close()
 	return lines
 
-def read_temp():
+def get_data():
 	lines = read_temp_raw()
 	while lines[0].strip()[-3:] != 'YES':
 		time.sleep(0.2)
@@ -33,14 +36,24 @@ def read_temp():
 		outdoor_temp = get_yahoo_temp()
 		return epoch, indoor_temp, outdoor_temp
 
-def write_temp(line):
-	with open('temperature.txt', 'a') as output_file:
-		output_file.write(line + '\n')
+def register_temperature():
+	epoch, indoor_temp, outdoor_temp = get_data()
+	sample_datetime = datetime.datetime.fromtimestamp(epoch)
+	date = sample_datetime.strftime('%Y-%m-%d')
+	
+	with open(os.path.join('logs', date), 'a') as output_file:
+		temp_writer = csv.writer(output_file, delimiter=',')
+		temp_writer.writerow([epoch,
+							'%.02f' % indoor_temp,
+							'%.02f' % outdoor_temp])
 
-while True:
-	try:
-		write_temp('%d,%0.2f,%0.2f' % read_temp())
-	except Exception as inst:
-		sys.stderr.write('%s: %s' % (type(inst), inst))
-	time.sleep(600)
+def main():
+	while True:
+		try:
+			register_temperature()
+		except Exception as inst:
+			sys.stderr.write('%s: %s' % (type(inst), inst))
+		time.sleep(600)
 
+if __name__ == '__main__':
+	main()
